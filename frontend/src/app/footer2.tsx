a0d25c9dde9f5273a4cba9bb8bc4ba5d041ad22c
tuvshuu <148052892+fenrir434@users.noreply.github.com>
2024-04-11 22:05:12 +0800

    Footer done
    Footer Apr11 22:05 Done fully

---
+++ frontend/src/app/footer2.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+
+function Footer2() {
+    const roundedIconStyle = {
+        backgroundColor: "#151875",
+        borderRadius: "50%",
+        padding: "5px",
+    };
+
+    return (
+        <footer className="flex justify-center items-center px-16 py-4 w-full text-base font-semibold text-gray-400 whitespace-nowrap bg-violet-100 max-md:px-5 max-md:max-w-full">
+            <div className="flex gap-5 justify-between max-w-full w-[960px] max-md:flex-wrap">
+                <div className="my-auto">©ecommerce</div>
+                <div className="flex gap-4">
+                    <div style={roundedIconStyle}>
+                        <a href="https://www.facebook.com">
+                            <Facebook style={{ color: "white" }} />
+                        </a>
+                    </div>
+                    <div style={roundedIconStyle}>
+                        <a href="https://www.instagram.com">
+                            <Instagram style={{ color: "white" }} />
+                        </a>
+                    </div>
+                    <div style={roundedIconStyle}>
+                        <a href="https://www.twitter.com">
+                            <Twitter style={{ color: "white" }} />
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer2;
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,14 @@
-
 import Header from "./header";
-
 import Dashboard from "./dashboard";
+import Footer from "./footer";
+import Footer2 from "./footer2";
 export default function Home() {
-  return (
-    <Header/>
- <Dashboard/>
-  );
+    return (
+        <>
+            <Header />
+            <Dashboard />
+            <Footer />
+            <Footer2 />
+        </>
+    );
 }
